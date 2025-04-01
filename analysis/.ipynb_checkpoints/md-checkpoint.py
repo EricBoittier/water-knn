5c4d6430f72f12d72e5689f9cef965d7cc5b7814
EricBoittier <eric.boittier@icloud.com>
2025-04-01 11:44:10 +0200

    hexamers

--- analysis/.ipynb_checkpoints/md-checkpoint.py
+++ analysis/.ipynb_checkpoints/md-checkpoint.py
@@ -1,4 +1,105 @@
-from ff_energy.simulations import charmm, plots
+import os
+import pandas as pd
+
+DYNASTART = "CHARMM>    DYNA"
+DYNAEXTERN = "DYNA EXTERN>"
+DYNA = "DYNA>"
+DYNAPRESS = "A PRESS>"
+
+
+def read_charmm_log(path, title=None):
+    """Read a charmm log file and return the lines"""
+    with open(path, "r") as f:
+        lines = f.readlines()
+    df = read_charmm_lines(lines)
+    df["path"] = path
+
+    if title:
+        df["title"] = title
+    else:
+        df["title"] = path.replace("/", "_")
+
+    return df
+
+
+def read_pressures(pressures):
+    try:
+        x = pressures
+        volume = float(x[67:73])
+        pressi = 0 #float(x[55:68])
+        presse = 0 #float(x[40:53])
+        return volume, pressi, presse
+    except ValueError:
+        return None, None, None
+
+
+def read_energies(energies):
+    x = energies
+    T = float(x[70:])
+    TOTE = float(x[27:40])
+    E = float(x[41:70])
+    t = float(x[16:27])
+    return t, T, TOTE, E
+
+
+
+# DYNA EXTERN>     1526.65445  -9553.35670      0.00000      0.00000      0.00000
+
+
+def read_extern(externs):
+
+    x = externs
+    vdw = float(x[13:27])
+    elec = float(x[27:41])
+    user = float(x[41:55])
+    return vdw, elec, user
+
+
+
+def read_charmm_lines(lines):
+    """Read a list of lines and return the complexation energy"""
+    dynamics = []
+    pressures = []
+    energies = []
+    externs = []
+    #  dcd files
+    dcds = []
+    starts = 0
+    for line in lines:
+        if DYNASTART in line.upper():
+            dyna_name = " ".join(line.split()[1:4])
+            dynamics.append("{}: ".format(starts) + dyna_name)
+            starts += 1
+        if DYNAEXTERN in line:
+            externs.append([*read_extern(line), dynamics[-1], dcds[-1]])
+        if DYNA in line:
+            energies.append([*read_energies(line)])
+        if DYNAPRESS in line:
+            pressures.append([*read_pressures(line)])
+        #  record the name of the dcd file
+        if line.startswith(" CHARMM>    OPEN WRITE") and ".dcd" in line:
+            dcdfilename = [_ for _ in line.split() if _.endswith(".dcd")]
+            assert len(dcdfilename) == 1
+
+            dcdfilename = os.path.basename(dcdfilename[0])
+            #  if the dcd file is already in the list, remove it
+            if dcdfilename in dcds:
+                pass
+            else:
+                print(dcdfilename)
+                dcds.append(dcdfilename)
+
+    df = pd.concat(
+        [
+            pd.DataFrame(externs, columns=["vdw", "elec", "user", "dyna", "dcd"]),
+            pd.DataFrame(energies, columns=["time", "temp", "tot", "energy"]),
+            pd.DataFrame(pressures, columns=["volume", "pressi", "presse"]),
+        ],
+        axis=1,
+    )
+
+    return df
+
 import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import Path
@@ -17,15 +118,15 @@ ureg = UnitRegistry()
 #  constants
 Avogadro_const = 6.02214129 * 10**23 * ureg("mol^-1")  # % mol-1
 
-# def get_density(volume, N_res, MW):
-#     """
-#     get the density of the simulation for a given volume, number of molecules and
-#     molecular weight :param volume: m^3 :param N_res: number :param MW: g/mol
-#     :return: density in g/m^3
-#     """
-#     MW = MW * ureg("g/mol")
+def get_density(volume, N_res, MW):
+    """
+    get the density of the simulation for a given volume, number of molecules and
+    molecular weight :param volume: m^3 :param N_res: number :param MW: g/mol
+    :return: density in g/m^3
+    """
+    MW = MW * ureg("g/mol")
 
-#     return (N_res * MW) / (Avogadro_const * volume)
+    return (N_res * MW) / (Avogadro_const * volume)
 
 # def get_dG_(total_energy, nwater=2000, usingle = -228.20148):
 #     tot = total_energy.mean()
@@ -106,7 +207,7 @@ def get_job_data(files):
     for _ in files:
         # try:
         print(_)
-        cl = charmm.read_charmm_log(_)
+        cl = read_charmm_log(_)
         dens = print_vol_error(cl)
         cl["dens"] = dens
         cls_.append(cl)
@@ -115,3 +216,61 @@ def get_job_data(files):
         #     print(e)
     return {k: v for k,v in zip(jobids, cls_)}
 
+import MDAnalysis as mda
+import MDAnalysis.analysis.msd as msd
+from pathlib import Path
+import numpy as np
+from scipy.stats import linregress
+import matplotlib.pyplot as plt
+
+
+def load_unwrapped_dcd(base):
+    psfpath = base / "water.psf"
+    dcdpath = base / "dcd" / "unwrapped.dcd"
+    u = mda.Universe(psfpath, dcdpath)
+    return u
+
+def calc_msd(u):
+    MSD = msd.EinsteinMSD(u, select='type OT', msd_type='xyz', fft=True)
+    NSKIP = 1
+    MSD.run(0, -1, NSKIP)
+    return MSD
+
+def fit_D_from_MSD(MSD):
+    msd_results =  MSD.results.timeseries
+    nframes = MSD.n_frames
+    timestep = NSKIP * 10_000 * 0.0002 # this needs to be the actual time between frames
+    lagtimes = np.arange(nframes)*timestep # make the lag-time axis
+
+    start_time = 0
+    start_index = int(start_time/timestep)
+    end_index = -1
+
+    linear_model = linregress(
+        lagtimes[start_index:end_index],
+        msd_results[start_index:end_index]
+    )
+    slope = linear_model.slope
+    error = linear_model.rvalue
+    D = slope * 1/(2*MSD.dim_fac)
+    return {"lagtimes": lagtimes, "msd": msd_results, "D": D}
+
+def plot_self_diffusion(results):
+    
+    lagtimes = results["lagtimes"]
+    msd_results = results["msd"]
+    D = results["D"]
+    
+    exact = lagtimes*6
+    fig = plt.figure()
+    ax = plt.axes()
+    ax.plot(lagtimes, msd_results, c="black", ls="-", label=r'Observed')
+    plt.plot(lagtimes[start_index:], 
+             lagtimes[start_index:]*slope + linear_model.intercept, c="r", label="Fit")
+    plt.title(f"$D = ${D / 0.1 :.2f}")
+    ax.plot(lagtimes, exact, c="black", ls="--", label=r'3D Brownian Motion')
+    plt.legend()
+    plt.xlabel("Time [ps]", fontsize=20)
+    plt.ylabel("MSD [$\mathrm{\AA}^{3}$]", fontsize=20)
+    # plt.savefig("msd_water_nn.pdf")
+    # plt.show()
